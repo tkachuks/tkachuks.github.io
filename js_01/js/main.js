66e65fa6ebbd194610d7ce633b4c2680a04b00b1
Sergey Tkachuk <serhiytkachuk88@gmail.com>
2017-05-12 22:05:29 +0300

    fixed js

--- js_01/js/main.js
+++ js_01/js/main.js
@@ -1,21 +1,69 @@
-'use strict'
-function pow (num, n) {
-  'use strict'
-  var result = num;
-  for (var i = 1; i < n; i++) {
-  result *= num;
+// Завдання 1
+(function() {
+  'use strict';
+
+while (true) {
+  var num = +prompt('Введите число','');
+  var n = +prompt('Возвести в степень','');
+  if ( !isNaN(num) && !isNaN(n)){
+    alert (pow(num, n));
+    break;
+  }else {
+    alert('Введён не числовой тип данных');
   }
-  return result;
 }
 
-  var num = prompt('Введите число','');
-  var n = prompt('Возвести в степень','');
-
+function pow (num, n) {
+  var result = num;
   if (n === 0) {
     result = 1;
+    return result;
   } else if (n < 0) {
-    alert ('Степень не может принимать отрицательные значения');
-    result = null;
-  } else {
-    alert (pow (num, n));
+    for (var i = 1; i < n; i++) {
+    result *= num;
   }
+    result = 1 / result;
+    return result;
+  } else {
+    for (var j = 1; j < n; j++) {
+    result *= num;
+  }
+  return result;
+  }
+}
+})();
+
+
+
+// Завдання 2
+
+(function() {
+  'use strict';
+  var users = [];
+  for (var i = 0; i < 5; i++) {
+    users[i] = prompt ('Введите имена пользователей:', '');
+  }
+
+  users.forEach(function (element, index, users) {
+    users[index] = element.toUpperCase();
+  });
+  console.log(users);
+
+  var admin = prompt ('Введите ваше имя:', '');
+  var upperAdmin = admin.toUpperCase();
+  console.log(upperAdmin);
+
+  for (var j = 0; j < users.length; j++) {
+    if (users[j] === upperAdmin) {
+      var result = upperAdmin;
+    }
+    
+  }
+
+  if (result) {
+    alert (upperAdmin + ', Вы успешно вошли');
+  } else {
+    alert ('Ошибка. Введено не верное имя пользователя');
+  }
+
+})();
