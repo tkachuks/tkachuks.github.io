1331cb3ba18d5da2e8e7827b3588d064ec2b759d
tkachuks <serhiytkachuk88@gmail.com>
2020-01-30 16:52:46 +0200

    change scope in manifest

--- SW/app.js
+++ SW/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js')
+  navigator.serviceWorker.register('./SW/service-worker.js')
     .then(reg => console.log('service worker registered', reg))
     .catch(err => console.log('service worker not registered', err))
 }
